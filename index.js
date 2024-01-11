33478a3626fb5aec6f8ffee8ceecf9347412f409
Daniel Victor Silva Bonfim <112987011+dasilvabonfim@users.noreply.github.com>
2024-01-11 00:12:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+function relogio(){
 
-function horarelogio()
-{
-  const horario = new Date();
-  relogio.innerHTML = horario.toLocaleTimeString();
+  const relogio = document.querySelector('.relogio');
+  let segundos = 0;
+  let temporizador;
+
+
+  function iniciarelogio()
+  {
+    temporizador = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = getHoraDosSegundos(segundos);
+    },1000);
+  }
+
+  document.addEventListener('click', function(e){
+    const elemento = e.target;
+
+    if (elemento.classList.contains('iniciar')){
+      relogio.classList.remove('pausado');
+      relogio.classList.add('iniciado');
+      clearInterval(temporizador);
+      iniciarelogio();
+    }
+
+    if (elemento.classList.contains('pausar')){
+      clearInterval(temporizador);
+      relogio.classList.remove('iniciado');
+      relogio.classList.add('pausado');
+    }
+
+    if (elemento.classList.contains('zerar')){
+      clearInterval(temporizador);
+      relogio.classList.remove('pausado');
+      relogio.classList.remove('iniciado');
+      relogio.innerHTML = '00:00:00'
+      segundos = 0;
+    }
+  })
+
+
+  function getHoraDosSegundos(segundos)
+  {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {hour12: false, timeZone: 'UTC'});
+  }
+
 }
 
-iniciar.addEventListener('click', function() {
-    horario = setInterval(function (){
-      horarelogio();
-    },1000);
-  });
 
-pausar.addEventListener('click', function(){
-  clearInterval(horario);
-  relogio.innerHTML = '00:00:00'
-});
+relogio();
